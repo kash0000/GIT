7680629fd4b0f6cbe960cdc63cc324ea6c0a0cf4
kash0000 <137502138+kash0000@users.noreply.github.com>
2023-09-18 02:53:47 +0530

    Update up_script.js

--- up_script.js
+++ up_script.js
@@ -1,16 +1,24 @@
-// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all the table rows
+  const rows = document.querySelectorAll("tbody tr");
 
-// Function to make rows with 'SLA Breached' column as 'Yes' flash red
-function flashSLAStatus() {
-    const rows = document.querySelectorAll('tbody tr');
-    rows.forEach((row) => {
-        const slaBreachedCell = row.querySelector('td:nth-child(3)'); // Assuming 'SLA Breached' is the third column (index 2)
-        if (slaBreachedCell.textContent.trim() === 'Yes') {
-            // Add the 'flash-red' class to the row
-            row.classList.add('flash-red');
-        }
-    });
-}
+  // Iterate through the rows
+  rows.forEach(function (row) {
+    // Get the value of the "SLA Breached" column in the current row
+    const slaBreachedValue = row.querySelector("td:last-child").textContent.trim();
 
-// Call the function on page load
-window.addEventListener('load', flashSLAStatus);
+    // Check if "SLA Breached" is set to "No"
+    if (slaBreachedValue === "No") {
+      // Add a CSS class to flash the row green
+      flashGreen(row);
+    }
+  });
+
+  // Function to add and remove the CSS class for flashing
+  function flashGreen(element) {
+    element.classList.add("flash-green");
+    setTimeout(function () {
+      element.classList.remove("flash-green");
+    }, 1000); // Flash for 1 second (adjust as needed)
+  }
+});
