2228e1473f2d9a01834b47f397830df727cbcb7f
kash0000 <137502138+kash0000@users.noreply.github.com>
2024-04-05 10:56:27 +0530

    Update upld3.ts
    added all

--- upld3.ts
+++ upld3.ts
@@ -1,3 +1,5 @@
+#file-upload.component.ts:
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -21,3 +23,58 @@ export class FileUploadComponent {
     );
   }
 }
+
+#file-upload.service.ts:
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor(private http: HttpClient) { }
+
+  uploadFile(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post<any>('http://your-backend-url/upload', formData);
+  }
+}
+
+
+#python 
+
+  from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    data = pd.read_excel(file)
+    
+    # Process data here
+    
+    # Save processed data to a new file
+    processed_file_path = 'processed_data.xlsx'
+    data.to_excel(processed_file_path, index=False)
+    
+    return jsonify({'message': 'File uploaded and processed successfully', 'file_path': processed_file_path})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#python1
+
+  from sqlalchemy import create_engine
+import pandas as pd
+
+engine = create_engine('your-database-connection-string')
+
+data = pd.read_excel('processed_data.xlsx')
+data.to_sql('your_table_name', engine, if_exists='append', index=False)
