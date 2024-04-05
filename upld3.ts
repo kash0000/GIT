175e19e2e421facab7eb6a3f7089abaa05477edc
kash0000 <137502138+kash0000@users.noreply.github.com>
2024-04-05 10:50:45 +0530

    Create upld3.ts
    gp T

---
+++ upld3.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent {
+
+  constructor(private fileUploadService: FileUploadService) { }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    this.fileUploadService.uploadFile(file).subscribe(
+      (response) => {
+        console.log('File uploaded successfully:', response);
+      },
+      (error) => {
+        console.error('Error uploading file:', error);
+      }
+    );
+  }
+}
