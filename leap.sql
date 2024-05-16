76e3bcc377b8e21c491a5b98be8efc7fd294e44f
kash0000 <137502138+kash0000@users.noreply.github.com>
2024-05-16 17:36:05 +0530

    Update leap.sql

--- leap.sql
+++ leap.sql
@@ -1,19 +1,19 @@
 DECLARE
     v_date DATE := TO_DATE('30-Apr-2024', 'DD-MON-YYYY'); -- Example date
-    v_leap_year NUMBER;
     days_in_year NUMBER;
+    v_year NUMBER;
 BEGIN
-    -- Determine if the year is a leap year
-    SELECT COUNT(*)
-    INTO v_leap_year
-    FROM DUAL
-    WHERE TO_NUMBER(TO_CHAR(v_date, 'YYYY')) = EXTRACT(YEAR FROM ADD_MONTHS(LAST_DAY(v_date), 2));
+    -- Extract the year from the date
+    v_year := TO_NUMBER(TO_CHAR(v_date, 'YYYY'));
 
-    -- Use a case statement to determine the number of days in the year
-    days_in_year := CASE
-                        WHEN v_leap_year = 1 THEN 366 -- Leap year has 366 days
-                        ELSE 365    -- Non-leap year has 365 days
-                    END;
+    -- Determine if the year is a leap year
+    IF (MOD(v_year, 4) = 0 AND MOD(v_year, 100) != 0) OR (MOD(v_year, 400) = 0) THEN
+        -- Leap year has 366 days
+        days_in_year := 366;
+    ELSE
+        -- Non-leap year has 365 days
+        days_in_year := 365;
+    END IF;
 
     -- Print the value of the variable
     DBMS_OUTPUT.PUT_LINE('Days in year: ' || days_in_year);
